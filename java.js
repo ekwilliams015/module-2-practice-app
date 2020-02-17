9c2e92c5dbdaa3ad907ef22dfb8a03631b760b32
Elijah Williams <ekwilliams015@gmail.com>
2020-02-17 16:27:59 -0600

    added css

--- java.js
+++ java.js
@@ -4,10 +4,10 @@ function calculate(){
     let userTotal = parseFloat(document.getElementById("total").value);
     
     if (userTotal == ""){
-        alert("ERROR! Need More Info.");
+        alert("Need More Info.");
     }
     if (userRate == ""){
-        alert("ERROR! Need More Info.")
+        alert("Need More Info.")
     }
     document.getElementById("tipAmount").innerHTML = "$" + userTotal*userRate;
     document.getElementById("taxAmount").innerHTML = "$" + 0.055*userTotal;
