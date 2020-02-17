0eae350fa4fd2b89a363e401656b4aa22891d704
Elijah Williams <ekwilliams015@gmail.com>
2020-02-17 15:03:20 -0600

    add css

--- java.js
+++ java.js
@@ -1,1 +1,15 @@
+function calculate() {
+    let rateInput = parseFloat(document.getElementById("rate").value);
 
+    let rateInput = parseFloat(document.gerElementId("total".value));
+if (totalInput == ""){
+    alert("Error, please fill out all boxes.")
+}
+if (rateInput == ""){
+    alert("Error, please fill out all boxes.")
+}
+document.getElementById("tipAmount").innerHTML = "$" + totalInput*rateInput;
+document.getElementById("taxAmount").innerHTML = "$" + totalInput*0.055;
+document.getElementById("TotalAmount").innerHTML = "$" + (totalInput + (totalInput*rateInput) + (totalInput*0.055));
+return false;
+}
