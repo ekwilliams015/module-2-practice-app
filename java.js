b98ed270c2c32d2ca18caf93d7560bdd0a5c1c7f
Elijah Williams <ekwilliams015@gmail.com>
2020-02-17 16:01:01 -0600

    fixed formatting

--- java.js
+++ java.js
@@ -1,15 +1,16 @@
+  
 function calculate(){
     let rateInput = parseFloat(document.getElementById("rate").value);
-    let totalInput = parseFloat(document.getElementId("total").value);
-
-if (totalInput == ""){
-    alert("Error, please fill out all boxes.");
-}
-if (rateInput == " "){
-    alert("Error, please fill out all boxes.");
-}
-document.getElementById("tipAmount").innerHTML = "$" + totalInput*rateInput;
-document.getElementById("taxAmount").innerHTML = "$" + totalInput*0.055;
-document.getElementById("TotalAmount").innerHTML = "$" + (totalInput + (totalInput*rateInput) + (totalInput*0.055));
-return false;
+    let totalInput = parseFloat(document.getElementById("total").value);
+    
+    if (totalInput == ""){
+        alert("ERROR! Need More Info.");
+    }
+    if (rateInput == ""){
+        alert("ERROR! Need More Info.")
+    }
+    document.getElementById("tipAmount").innerHTML = "$" + totalInput*rateInput;
+    document.getElementById("taxAmount").innerHTML = "$" + totalInput*0.055;
+    document.getElementById("TotalAmount").innerHTML = "$" + (totalInput + (totalInput*rateInput) + (totalInput*0.055));
+    return false;
 }
