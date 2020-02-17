c161f677676317528d35455143465a1b8e4e8e5d
Elijah Williams <60112876+ekwilliams015@users.noreply.github.com>
2020-02-17 16:09:12 -0600

    changed variable names

--- java.js
+++ java.js
@@ -1,16 +1,16 @@
   
 function calculate(){
-    let rateInput = parseFloat(document.getElementById("rate").value);
-    let totalInput = parseFloat(document.getElementById("total").value);
+    let userRate = parseFloat(document.getElementById("rate").value);
+    let userTotal = parseFloat(document.getElementById("total").value);
     
-    if (totalInput == ""){
+    if (userTotal == ""){
         alert("ERROR! Need More Info.");
     }
-    if (rateInput == ""){
+    if (userRate == ""){
         alert("ERROR! Need More Info.")
     }
-    document.getElementById("tipAmount").innerHTML = "$" + totalInput*rateInput;
-    document.getElementById("taxAmount").innerHTML = "$" + totalInput*0.055;
-    document.getElementById("TotalAmount").innerHTML = "$" + (totalInput + (totalInput*rateInput) + (totalInput*0.055));
+    document.getElementById("tipAmount").innerHTML = "$" + userTotal*userRate;
+    document.getElementById("taxAmount").innerHTML = "$" + 0.055*userTotal;
+    document.getElementById("TotalAmount").innerHTML = "$" + (userTotal + (userTotal*userRate) + (userRate*0.055));
     return false;
 }
